e4cbd697f1aed2ef2a13c3fac8de029a9a25956e
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-09-30 12:13:22 +0200

    Navbar components with menuopening functions, style like in figma, hamburger in circles with black hover and logo size was not wanting to go smaller but fixed with important

--- lauraocampo-portfolio/src/components/Navbar.jsx
+++ lauraocampo-portfolio/src/components/Navbar.jsx
@@ -1,22 +1,57 @@
-import Logo from "../assets/images/Favicon/Logo-LO-white.png";
+import Logo from "../assets/images/Logo/Logo-portfolio.png";
 import { navLinks } from "../../constants/index.js";
+import { useState } from "react";
+
 function Navbar() {
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [isClosing, setIsClosing] = useState(false);
+
+	const handleMenuToggle = () => {
+		if (menuOpen) {
+			setIsClosing(true);
+			setTimeout(() => {
+				setMenuOpen(false);
+				setIsClosing(false);
+			}, 800);
+		} else {
+			setMenuOpen(true);
+		}
+	};
+
 	return (
 		<nav className="navbar">
-			<div>
+			<div className="navbar-left">
 				<p>2025.</p>
+			</div>
+
+			<div className="navbar-center">
 				<a href="#home">
 					<img src={Logo} alt="logo" className="logo" />
-					<p>campo</p>
 				</a>
-				<ul>
-					{navLinks.map((link) => (
-						<li key={link.id}>
-							<a href={link.id}>{link.name}</a>
-						</li>
-					))}
-				</ul>
 			</div>
+
+			<div className="navbar-right">
+				<button className={`menu-button ${menuOpen ? "active" : ""}`} onClick={handleMenuToggle}>
+					Menu
+					<div className="menu-icon">
+						<span></span>
+						<span></span>
+						<span></span>
+					</div>
+				</button>
+			</div>
+
+			{menuOpen && (
+				<div className={`desktop-menu ${isClosing ? "closing" : "open"}`}>
+					<ul>
+						{navLinks.map((link) => (
+							<li key={link.id}>
+								<a href={link.id}>{link.name}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</nav>
 	);
 }
