acd1b38ad4a9947834a32867d6b959825258731e
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2025-10-05 21:25:34 +0200

    In pages homebutton no redirected to 404

--- lauraocampo-portfolio/constants/index.js
+++ lauraocampo-portfolio/constants/index.js
@@ -1,5 +1,5 @@
 export const navLinks = [
-	{ name: "Home", id: "/" },
+	{ name: "Home", id: "#" },
 	{ name: "About me", id: "#about" },
 	{ name: "Skills", id: "#skills" },
 	{ name: "Projects", id: "#projects" },
